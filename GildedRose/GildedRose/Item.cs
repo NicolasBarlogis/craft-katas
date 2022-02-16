cf7599621dd544f46c43eb8cefa188c9dd1d4725
Nicolas Barlogis <nicolas.barlogis@cdbdx.biz>
2022-02-16 10:39:13 +0100

    feat: added gilded rose kata

---
+++ GildedRose/GildedRose/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name + ", " + this.SellIn + ", " + this.Quality;
+        }
+    }
+}
---
+++ GildedRose/README.md
@@ -0,0 +1,6 @@
+# Gilded Rose Kata
+Updated template (.Net 6) from [Emily Bach's version](https://github.com/emilybache/GildedRose-Refactoring-Kata) of the kata.
+
+## How To
+You are tasked with adding new functionnality to legacy code ([see requirement file](requirements.txt)).
+You may refactor the existing code, add test coverage and adding the new functionnality.
