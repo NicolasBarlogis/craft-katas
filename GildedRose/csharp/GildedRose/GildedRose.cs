f8c45bafd21c341d51aea46cac72514f1424d27f
Valentin Gérard <valentin.gerard@cdbdx.biz>
2022-03-03 17:30:08 +0100

    Removing code complexity

--- GildedRose/csharp/GildedRose/GildedRose.cs
+++ GildedRose/csharp/GildedRose/GildedRose.cs
@@ -29,77 +29,57 @@
                 }
                 else
                 {
-                    if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality -= 1;
-                            }
-                        }
-                    }
-                    else
-                    {
+
                         if (item.Quality < 50)
                         {
                             item.Quality += 1;
 
-                            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                            if (item.SellIn < 11 && item.Quality < 50)
                             {
-                                if (item.SellIn < 11)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality += 1;
-                                    }
-                                }
+                                item.Quality += 1;
+                            }
 
-                                if (item.SellIn < 6)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality += 1;
-                                    }
-                                }
+                            if (item.SellIn < 6 && item.Quality < 50)
+                            {
+                                item.Quality += 1;
                             }
                         }
-                    }
 
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
                         item.SellIn -= 1;
-                    }
 
-                    if (item.SellIn >= 0)
-                    {
-                        continue;
-                    }
-
-                    if (item.Name != "Aged Brie")
-                    {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.SellIn >= 0)
                         {
-                            if (item.Quality <= 0)
+                            continue;
+                        }
+
+                        item.Quality -= item.Quality;
+                    }
+                    else
+                    {
+                        if (item.Name == "Sulfuras, Hand of Ragnaros")
+                        {
+                            if (item.SellIn >= 0 || item.Quality <= 0)
                             {
                                 continue;
-                            }
-
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality -= 1;
                             }
                         }
                         else
                         {
-                            item.Quality -= item.Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality += 1;
+                            if (item.Quality > 0)
+                            {
+                                item.Quality -= 1;
+                            }
+
+                            item.SellIn -= 1;
+
+                            if (item.SellIn >= 0 || item.Quality <= 0)
+                            {
+                                continue;
+                            }
+
+                            item.Quality -= 1;
                         }
                     }
                 }
