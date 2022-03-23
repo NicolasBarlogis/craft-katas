d5be210ce410a2470d3d0461e74c131fb400c91b
Audrey Comlan-Cataria <audrey.comlan-cataria@ext.cdiscount.com>
2022-03-23 11:46:17 +0100

    test: init test for Game

--- Bowling/react/src/test/App.test.js
+++ Bowling/react/src/test/App.test.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import App from '../App';
-import Enzyme, {shallow, mount} from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import ScoreBoard from '../ScoreBoard';
-import RollButtons from '../RollButtons';
+import React from "react";
+import App from "../App";
+import Enzyme, { shallow, mount } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import ScoreBoard from "../ScoreBoard";
+import RollButtons from "../RollButtons";
 
 beforeAll(() => {
   Enzyme.configure({ adapter: new Adapter() });
 });
 
-
-describe('The App renders correctly', () => {
-  it('renders without crashing', () => {
+describe("The App renders correctly", () => {
+  it("renders without crashing", () => {
     shallow(<App />);
   });
 
-  it('renders a ScoreBoard component', () => {
+  it("renders a ScoreBoard component", () => {
     const wrapper = setup();
     expect(wrapper.find(ScoreBoard)).toBeDefined();
   });
+});
+
+it("A game should have 10 rounds", () => {
+  //Arrange
+  const game = new Game();
+
+  //Act
+
+  //Assert
+  expect(game.rounds).to.have.lengthOf(10);
 });
 
 function setup() {
