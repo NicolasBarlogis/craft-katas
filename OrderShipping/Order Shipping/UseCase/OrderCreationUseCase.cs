5a5d5b33badb4c37eefad421fae8ba2224577af2
sandra <60742092+sandra33660@users.noreply.github.com>
2022-04-01 17:04:43 +0200

    creation use case move exception in repository

--- OrderShipping/Order Shipping/UseCase/OrderCreationUseCase.cs
+++ OrderShipping/Order Shipping/UseCase/OrderCreationUseCase.cs
@@ -30,29 +30,21 @@ namespace OrderShipping.UseCase
             foreach (var itemRequest in request.Requests)
             {
                 var product = _productCatalog.GetByName(itemRequest.ProductName);
+                var unitaryTax = Round((product.Price / 100m) * product.Category.TaxPercentage);
+                var unitaryTaxedAmount = Round(product.Price + unitaryTax);
+                var taxedAmount = Round(unitaryTaxedAmount * itemRequest.Quantity);
+                var taxAmount = Round(unitaryTax * itemRequest.Quantity);
 
-                if (product == null)
+                var orderItem = new OrderItem
                 {
-                    throw new UnknownProductException();
-                }
-                else
-                {
-                    var unitaryTax = Round((product.Price / 100m) * product.Category.TaxPercentage);
-                    var unitaryTaxedAmount = Round(product.Price + unitaryTax);
-                    var taxedAmount = Round(unitaryTaxedAmount * itemRequest.Quantity);
-                    var taxAmount = Round(unitaryTax * itemRequest.Quantity);
-
-                    var orderItem = new OrderItem
-                    {
-                        Product = product,
-                        Quantity = itemRequest.Quantity,
-                        Tax = taxAmount,
-                        TaxedAmount = taxedAmount
-                    };
-                    order.Items.Add(orderItem);
-                    order.Total += taxedAmount;
-                    order.Tax += taxAmount;
-                }
+                    Product = product,
+                    Quantity = itemRequest.Quantity,
+                    Tax = taxAmount,
+                    TaxedAmount = taxedAmount
+                };
+                order.Items.Add(orderItem);
+                order.Total += taxedAmount;
+                order.Tax += taxAmount;
             }
 
             _orderRepository.Save(order);
